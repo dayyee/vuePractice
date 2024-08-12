6501b50dfc12c519813f8f1e304a912e2e28922c
daye <dy92520@gmail.com>
2024-08-12 13:54:01 +0900

    vuestagram

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,13 @@
+<template>
+  <div><Post v-for="(data, i) in datas" :key="i" :data="datas[i]" /></div>
+</template>
+
+<script>
+import Post from "./Post";
+export default {
+  components: { Post },
+  props: { datas: Array },
+};
+</script>
+
+<style></style>
