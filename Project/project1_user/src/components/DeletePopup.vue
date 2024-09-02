9f0decb725b96d15561d5c958c3d10ed33835d4f
daye <dy92520@gmail.com>
2024-09-02 14:29:43 +0900

    240902 FE Complete_v1

---
+++ Project/project1_user/src/components/DeletePopup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="text-center pa-4">
+    <v-dialog :model-value="DeletePopupDialog" max-width="400" persistent>
+      <v-card text="삭제하시겠습니까?" title="🗑️ Delete">
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="closeDialog" variant="plain"> Cancle </v-btn>
+          <v-btn @click="DeleteInfo" variant="tonal"> Ok </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DeletePopup",
+  data() {
+    return {};
+  },
+  props: {
+    DeletePopupDialog: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    closeDialog() {
+      this.$emit("update:DeletePopupDialog", false);
+    },
+    DeleteInfo() {
+      console.log("삭제");
+      this.closeDialog();
+    },
+  },
+};
+</script>
+<style></style>
