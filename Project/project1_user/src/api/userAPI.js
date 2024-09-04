9acea04d16604d0e7f9c1b5f0442d904bebba554
daye <dy92520@gmail.com>
2024-09-04 18:39:19 +0900

    240904 FE EDIT ~ing

--- Project/project1_user/src/api/userAPI.js
+++ Project/project1_user/src/api/userAPI.js
@@ -7,6 +7,15 @@ export async function findAllUsers() {
     const result = await axios.get(`${url}/all`);
     return result.data;
   } catch {
-    console.log("error");
+    console.log("error: findAllUsers");
+  }
+}
+
+export async function findUserById(id) {
+  try {
+    const result = await axios.get(`${url}/${id}`);
+    return result.data;
+  } catch {
+    console.log("error: findUserById");
   }
 }
